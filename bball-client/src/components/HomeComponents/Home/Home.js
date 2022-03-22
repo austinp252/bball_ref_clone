a38fa0eb672b93cde743b7983cda92551119bf5b
austin252 <austinwp1@gmail.com>
2022-03-22 17:58:49 -0600

    test response

--- bball-client/src/components/HomeComponents/Home/Home.js
+++ bball-client/src/components/HomeComponents/Home/Home.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import {Link} from 'react-router-dom';
 
 import './Home.css';
 
 function Home() {
-  return(
-    <div className="home-container">
-        <div className="home">
-            <h1>
-                Test text
-            </h1>
-        </div>
-    </div>
-  );
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    //console.log('test');
+    fetch(`/api`)
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
+  if(data) {
+    return(
+      <div className="home-container">
+          <div className="home">
+              <h1>
+                  {data.message}
+              </h1>
+          </div>
+      </div>
+    );
+  } else{
+    return(
+      <div className="home-container">
+          <div className="home">
+              <h1>
+                  Test text
+              </h1>
+          </div>
+      </div>
+    );
+  }
 }
 
 export default Home;
