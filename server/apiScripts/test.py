86376e11f40742a3c0a5a588f1c6d320a1dce06a
austin252 <austinwp1@gmail.com>
2022-03-22 19:15:42 -0600

    test message script

--- server/apiScripts/test.py
+++ server/apiScripts/test.py
@@ -1,7 +1,4 @@
 import json
 import sys
 
-from nba_api.stats.endpoints import commonallplayers
-
-data = commonallplayers.CommonAllPlayers().common_all_players.get_json()
-print(data)
+print("test message")
