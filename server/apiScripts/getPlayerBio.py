bcc8609a6d2bd257269f80e9b83d1b710fb6bf0b
austin252 <austinwp1@gmail.com>
2022-04-16 21:53:59 -0600

    removed unused apiscripts

--- server/apiScripts/getPlayerBio.py
+++
@@ -1,8 +0,0 @@
-import sys
-import json
-
-from nba_api.stats.endpoints import playercareerstats
-playerID = sys.argv[1]
-playerCareerStatsData = playercareerstats.PlayerCareerStats(
-    per_mode36='PerGame', player_id=playerID).get_json()
-print(playerCareerStatsData)
--- server/apiScripts/getTeamBio.py
+++
@@ -1,7 +0,0 @@
-import sys
-import json
-
-from nba_api.stats.endpoints import teamestimatedmetrics
-teamID = sys.argv[1]
-teamData = teamestimatedmetrics.TeamEstimatedMetrics().team_estimated_metrics.get_dict()
-print(teamData)
