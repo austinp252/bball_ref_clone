a71f52be3062cfbdf59f0646cafd1cd2a28c0df5
austin252 <austinwp1@gmail.com>
2022-04-18 16:06:55 -0600

    styling for player button dropdowns

--- bball-client/src/components/PlayerComponents/PlayerPageSelectors/PlayerPageSelectors.js
+++ bball-client/src/components/PlayerComponents/PlayerPageSelectors/PlayerPageSelectors.js
@@ -12,7 +12,7 @@ function PlayerPageSelectors(props) {
   const seasons = props.seasons;
   const team = props.team.data ? props.team : null;
   const [showMenu, setShowMenu] = useState(false);
-  const [menuMode, setMenuMode] = useState('');
+  const [menuMode, setMenuMode] = useState('none');
 
   //console.log(`/players/:${params.letter}/:${params.id}`);
   useEffect(() => {
@@ -28,60 +28,75 @@ function PlayerPageSelectors(props) {
   });
   const handleMouseLeave = ((e) => {
         setShowMenu(false);
-        setMenuMode('');
+        setMenuMode('none');
   });
   console.log(team)
     return(
             <div className="info-selectors" onMouseEnter={(e)=> handleMouseEnter(e)} onMouseLeave={(e)=> handleMouseLeave(e)}>
                 <br />
-                <Link to={`/players/${lastInitial}/${playerID}/overview`}><button onMouseEnter={(e)=> handleMouseEnter(e, 'overview')} className={mode==='overview' ? 'active' : ''}>Overview</button></Link>
+                <Link to={`/players/${lastInitial}/${playerID}/overview`}><button onMouseEnter={(e)=> handleMouseEnter(e, 'none')} className={mode==='overview' ? 'active' : ''}>Overview</button></Link>
                 <button onMouseEnter={(e)=> handleMouseEnter(e, 'gamelog')} className={mode==='gamelog' ? 'active gamelog' : 'gamelog'}>Game Logs
                 </button>
                 <button onMouseEnter={(e)=> handleMouseEnter(e, 'generalSplits')} className={mode==='generalSplits' ? 'active generalSplits' : 'generalSplits'}>Splits</button>
                 <button onMouseEnter={(e)=> handleMouseEnter(e, 'shootingSplits')} className={mode==='shootingSplits' ? 'active shootingSplits' : 'shootingSplits'}>Shooting</button>
-                <button onMouseEnter={(e)=> handleMouseEnter(e, 'more')} className={mode==='more' ? 'active more' : 'more'}>More</button>
+                <button onMouseEnter={(e)=> handleMouseEnter(e, 'none')} className={mode==='more' ? 'active more' : 'more'}>More</button>
                 {
                     team &&
                     <button onMouseEnter={(e)=> handleMouseEnter(e, 'team')} className={mode==='team' ? 'active team' : 'team'}>2021-22 {team.data.resultSets[0].rowSet[0][2]}</button>
                 }
                 {
                     showMenu &&
-                    <div className="season-selector">
+                    <div className={`menu-selector ${menuMode === 'none' ? '' : 'show'}`}>
                     {
                         menuMode==='gamelog' &&
-                        seasons[0].data.map((season) => {
-                            return(
-                                <div className="menu-item">
-                                    <div className="season-item">
-                                        <Link to={`/players/${lastInitial}/${playerID}/gamelog/${season}`}>{season}</Link>
-                                    </div>
-                                </div>
-                            )
-                        })
+                        <div className="menu-item">
+                            <p>Game-by-game stat line for the player</p>
+                            <div className="season-items">
+                            {
+                                    seasons[0].data.map((season) => {
+                                    return(
+                                        <div className="season-item">
+                                            <Link to={`/players/${lastInitial}/${playerID}/gamelog/${season}`}>{season}</Link>
+                                        </div>
+                                    )
+                                })
+                            }
+                            </div>
+                        </div>
                     }
                     {
                         menuMode==='generalSplits' &&
-                        seasons[1].data.map((season) => {
-                            return(
-                                <div className="menu-item">
-                                    <div className="season-item">
-                                        <Link to={`/players/${lastInitial}/${playerID}/generalSplits/${season}`}>{season}</Link>
-                                    </div>
-                                </div>
-                            )
-                        })
+                        <div className="menu-item">
+                            <p>Player stats broken down into various categories; i.e. home/away, monthly, etc..</p>
+                            <div className="season-items">
+                                {
+                                    seasons[1].data.map((season) => {
+                                        return(
+                                            <div className="season-item">
+                                                <Link to={`/players/${lastInitial}/${playerID}/generalSplits/${season}`}>{season}</Link>
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </div>
+                        </div>
                     }
                     {
                         menuMode==='shootingSplits' &&
-                        seasons[1].data.map((season) => {
-                            return(
-                                <div className="menu-item">
-                                    <div className="season-item">
-                                        <Link to={`/players/${lastInitial}/${playerID}/shootingSplits/${season}`}>{season}</Link>
-                                    </div>
-                                </div>
-                            )
-                        })
+                        <div className="menu-item">
+                            <p>Player Shooting History</p>
+                            <div className="season-items">
+                                {
+                                    seasons[1].data.map((season) => {
+                                        return(
+                                            <div className="season-item">
+                                                <Link to={`/players/${lastInitial}/${playerID}/shootingSplits/${season}`}>{season}</Link>
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </div>
+                        </div>
                     }
                     {
                         menuMode==='team' &&
